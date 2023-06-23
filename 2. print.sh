d3368acdfe1365e90deb3650c5628e374a0a239d
Bhavanisankar <shankar4137@gmail.com>
2023-06-23 12:07:38 +0530

    color

--- 2. print.sh
+++ 2. print.sh
@@ -1,3 +1,11 @@
 # echo is used to print
 
 echo hellow world
+
+
+# to print message in color
+# echo -e "\e[COLmMESSAGE\e[0m
+
+echo -e \e[31m hello in red color\e[0m
+echo -e |\e[32m hello in green color\e[0m
+echo -e |\e[33m hello in  yellow color\e[0m
