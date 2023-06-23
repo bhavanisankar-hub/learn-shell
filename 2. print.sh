39e58ff45f29f8f1ab255a9c1c02507afda30f03
Bhavanisankar <shankar4137@gmail.com>
2023-06-23 12:08:59 +0530

    color

--- 2. print.sh
+++ 2. print.sh
@@ -7,5 +7,5 @@ echo hellow world
 # echo -e "\e[COLmMESSAGE\e[0m
 
 echo -e \e[31m hello in red color\e[0m
-echo -e |\e[32m hello in green color\e[0m
-echo -e |\e[33m hello in  yellow color\e[0m
+echo -e \e[32m hello in green color\e[0m
+echo -e \e[33m hello in  yellow color\e[0m
