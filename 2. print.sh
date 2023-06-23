034931a92775b70e7b600cb68fcd050af972a5c7
Bhavanisankar <shankar4137@gmail.com>
2023-06-23 12:11:19 +0530

    color

--- 2. print.sh
+++ 2. print.sh
@@ -6,6 +6,7 @@ echo hellow world
 # to print message in color
 # echo -e "\e[COLmMESSAGE\e[0m
 
-echo -e \e[31m hello in red color\e[0m
-echo -e \e[32m hello in green color\e[0m
-echo -e \e[33m hello in  yellow color\e[0m
+echo -e "\e[31m hello in red color\e[0m
+echo -e "\e[32m hello in green color\e[0m
+echo -e "\e[33m hello in  yellow color\e[0m
+
